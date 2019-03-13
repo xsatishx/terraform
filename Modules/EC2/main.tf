a55373f676c4af2a47ada3d08c412fcf5af7c5ec
Satish <balakrishnan.satish@accenture.com>
2019-03-13 12:42:32 +0800

    Modified

--- Modules/EC2/main.tf
+++ Modules/EC2/main.tf
@@ -1,21 +1,20 @@
 resource "aws_instance" "ec2" {
-  ami                         = "${var.ami}"
-  instance_type               = "${var.instance_type}"
-  iam_instance_profile        = "${var.iam_role}"
-  key_name                    = "${var.key_name}"
-  subnet_id 		              = "${element(var.subnet_id, count.index)}"
-  vpc_security_group_ids      = ["${var.sg_ec2_id}"]
+  ami                  = "${var.ami}"
+  instance_type        = "${var.instance_type}"
+  iam_instance_profile = "${var.iam_role}"
+  key_name             = "${var.key_name}"
+  subnet_id            = "${element(var.subnet_id, count.index)}"
+
   associate_public_ip_address = "${var.associate_public_ip_address}"
-  user_data                   = "${var.user_data}"
-  
+
   ## Insert EC2 ec2_tags
   tags {
-    Name                      = "${element(var.ec2_tag_name, count.index +1)}"
-    Company                   = "${var.ec2_tag_company}"
-    AppLevel                  = "${var.ec2_tag_applevel}"
-    AppName                   = "${var.ec2_tag_appname}"
-    Function                  = "${var.ec2_tag_function}"
-    AdminName                 = "${var.ec2_tag_adminname}"
-    AppType                   = "${var.ec2_tag_apptype}"
+    Name      = "${element(tag_name, count.index +1)}"
+    Company   = "${var.tag_company}"
+    AppLevel  = "${var.tag_applevel}"
+    AppName   = "${var.tag_appname}"
+    Function  = "${var.tag_function}"
+    AdminName = "${var.tag_adminname}"
+    AppType   = "${var.tag_apptype}"
   }
 }
--- main.tf
+++ main.tf
@@ -3,5 +3,12 @@ provider "aws" {
 }
 
 module "EC2" {
-  source = "git::https://github.com/xsatishx/terraform.git//Modules/EC2"
+  source        = "git::https://github.com/xsatishx/terraform.git//Modules/EC2"
+  tag_name      = "Satish"
+  tag_company   = "Satish"
+  tag_applevel  = "1"
+  tag_function  = "Test"
+  tag_apptype   = "NewApp"
+  tag_appname   = "MyApp"
+  tag_adminname = "Satish"
 }
