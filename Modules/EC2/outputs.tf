a2babc899d28f4a0cb3ba345d17d54bed20246d3
Satish <balakrishnan.satish@accenture.com>
2019-03-19 11:40:38 +0800

    test

--- Modules/EC2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_ids" {
-  description = "List of IDs of instances"
-  value       = ["${aws_instance.ec2.*.id}"]
-}
-
-output "instance_az" {
-  description = "List of AZs belonging to instance"
-  value       = ["${aws_instance.ec2.*.availability_zone}"]
-}
