e22d442a6291e085280f073e2a4f081738b6c52c
Balakrishnan <satishb@hpe.com>
2019-11-20 14:01:09 +0800

    changed the keypair and added parameter group and options group

--- Modules/sample/main.tf
+++ Modules/sample/main.tf
@@ -23,7 +23,6 @@ module "ec2_instance" {
   source                 = "github.com/xsatishx/terraform/Modules/EC2"
   vpc_security_group_ids = ["${module.vpc.vpc_security_group_ids}"]
   subnet_ids             = ["${module.vpc.intra_subnets}"]
-  ebs_count              = 0
   tag_environment  = "${var.tag_environment}"
   tag_adminname   = "${var.tag_adminname}"
   tag_createdby    = "${var.tag_createdby}"
